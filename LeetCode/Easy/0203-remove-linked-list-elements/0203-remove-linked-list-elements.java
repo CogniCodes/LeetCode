c1f958fcfe102654695290b3ab2afdd747476e7f
CogniCodes <shaunabraham377@gmail.com>
2026-09-02 23:40:13 +0530

    Time: 1 ms (96.95%), Space: 46.9 MB (97.44%) - LeetHub

---
+++ LeetCode/Easy/0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+    
+        ListNode dummy = new ListNode(0, head);
+        ListNode prev = dummy;
+
+        while (prev.next != null) {
+            if (prev.next.val == val) 
+                prev.next = prev.next.next;
+            else 
+                prev = prev.next;
+        }
+
+        return dummy.next;
+    }
+}
